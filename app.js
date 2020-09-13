fd2439681e595c2741d85f08bbc9dd160b2ec5b9
rancid <robmgraham13@yahoo.com>
2020-09-12 22:44:33 -0700

    added READme

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ function answers() {
     ]
 
 };
-
+render()
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
